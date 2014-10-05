a1100d9cfeeea7ea2a51524e66cfe1b24259f246
w.d.garey <whitewez@hotmail.com>
2014-10-05 19:37:38 -0400

    Main Changes:
    	-Added a function to read the contents of a Word document.

--- private/admin/model/model.php
+++ private/admin/model/model.php
@@ -36,6 +36,55 @@
     }
     
     /**
+     * Reads a word document and retursn the plain text.
+     * @param string $filename The file path of the word document.
+     * @return mixed The content of the word document, FALSE otherwise.
+     */
+    function GetWordDocContents($filename)
+    {
+        $striped_content = '';
+        $content = ''; 
+        
+        if(!$filename || !file_exists($filename))
+        {
+            return false; 
+        }
+        
+        $zip = zip_open($filename);
+
+        if (!$zip || is_numeric($zip))
+        {
+            return false;
+        }
+        
+        while ($zip_entry = zip_read($zip))
+        { 
+            if (zip_entry_open($zip, $zip_entry) == FALSE)
+            {
+                    continue;
+            }
+            
+            if (zip_entry_name($zip_entry) != "word/document.xml")
+            {
+                continue; 
+            }
+            
+            $content .= zip_entry_read($zip_entry, zip_entry_filesize($zip_entry)); 
+
+            zip_entry_close($zip_entry);
+
+        }
+
+        zip_close($zip);
+
+        $content = str_replace('</w:r></w:p></w:tc><w:tc>', " ", $content);
+        $content = str_replace('</w:r></w:p>', "\r\n", $content);
+        $striped_content = strip_tags($content);
+
+        return $striped_content;
+    }
+    
+    /**
      * Gets the number of times an answer has been submitted.
      * @param int $answerID The I.D. of the answer.
      * @return mixed The count if the statistic exists, or FALSE otherwise.
