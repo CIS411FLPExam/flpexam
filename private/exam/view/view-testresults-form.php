f0a98ecca8a47cefb195d45bff3666f850848dad
w.d.garey <whitewez@hotmail.com>
2014-10-07 12:16:32 -0400

    Main Changes:
    	-Fixed a typo on the test results student view page.

--- private/exam/view/view-testresults-form.php
+++ private/exam/view/view-testresults-form.php
@@ -5,7 +5,7 @@
 <h1>Test Results</h1>
 <div class="formGroup">
         <h3>Test Info</h3>
-        <h4>Thank you taking Clarion University's <?php echo(htmlspecialchars($languageName)); ?> placement test!</h4>
+        <h4>Thank you for taking Clarion University's <?php echo(htmlspecialchars($languageName)); ?> placement test!</h4>
         <?php if ($levelInfoID > 0) { ?>
             <div class="divider"></div>
             
